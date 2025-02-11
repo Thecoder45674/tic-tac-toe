32f6d0e70e14b2224a442f2f5d8621ee5def4fec
Thecoder45674 <farah.liban.abdi@gmail.com>
2025-02-11 17:29:14 +1100

    feat: Add initial structure for GameBoard, Cell, and GameController classes

---
+++ script.js
@@ -0,0 +1,26 @@
+/* 
+** The GameBoard represents the stat of the board
+** Each square holds a Cell
+** and expose placeToken method to  be able add Cells to square
+*/
+function GameBoard {
+
+}
+
+/*
+** A Cell represents one square on the board and can have one off
+** 0: no token currently in square
+** 1: Player One token
+** 2: Player Two token 
+*/
+function Cell {
+
+}
+
+/*
+** The GameController is responsible for controlling the folow state
+** of the game turns as well as the outcome of the game
+*/
+function GameController {
+
+}
